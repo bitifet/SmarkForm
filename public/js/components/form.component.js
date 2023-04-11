ace52ba9cd7aa955a2ea095eebc849be7f5d8475
Joan Miquel Torres Rigo <joanmi@gmail.com>
2023-04-11 19:57:42 +0200

    Component enhancement generalization.

--- public/js/components/form.component.js
+++ public/js/components/form.component.js
@@ -2,71 +2,20 @@
 // ============================
 
 import {SmartComponent} from "../lib/component.js";
-import {getRoots, validName} from "../lib/helpers.js";
-
-const sym_options = Symbol("Options");
-const re_valid_typename_chars = /^[a-z0-9_]+$/i;
+import {getRoots} from "../lib/helpers.js";
 
 export class form extends SmartComponent {
     render() {
         const me = this;
-        const templates = [];
 
         for (
             const node
             of getRoots(me.target, me.selector)
         ) {
-
-            // Sanityze and store options:{{{
-            let options = (node.dataset[me.property_name] || "").trim();
-            options = (
-                // Accept 'str' as shorthand for '{type: "str"}':
-                options.match(re_valid_typename_chars) ? {type: options}
-                // Otherwise it must be a valid JSON:
-                : JSON.parse(options)
-            );
-            const name = validName(
-                options.name
-                , node.getAttribute("name")
-            );
-            options.name = name;
-            options.parent = me;
-            node[sym_options] = options;
-            //}}}
-
-            // Classify:{{{
-            if (options.action) {
-                options.type ||= "action"; // Make type optional for actions.
-                if (options.type != "action") throw new Error(
-                    `Actions must be of type "action" but "${options.type}" given.`
-                );
-            } else if (typeof options.type != "string") {
-                throw new Error(
-                    `Invalid SmartFom item: type is mandatory for non action elements.`
-                );
-            };
-            templates.push(node);
-            //}}}
-
+            const newChild = me.enhance(node);
+            const {name} = newChild.options;
+            if (name) me.childs[name] = newChild;
         };
-
-        // Enhance components:{{{
-        for (const tpl of templates) {
-            const options = tpl[sym_options];
-            const ctrl = me.components[options.type];
-            if (! ctrl) throw new Error([
-                "Unimplemented SmartForm component controller:",
-                options.type,
-            ].join(" "));
-            ///////////////////////////////////////////////////////////
-            console.log("Creating child for " + me.options.name + " with name: " + options.name, tpl, options);
-            ///////////////////////////////////////////////////////////
-            me.childs[options.name] = new ctrl (
-                tpl
-                , options
-                , me
-            );
-        };//}}}
 
     };
 };
--- public/js/lib/component.js
+++ public/js/lib/component.js
@@ -3,8 +3,12 @@
 const componentTypes = {};
 
 import {Events} from "./events.js";
+import {validName} from "../lib/helpers.js";
 
 const sym_smart = Symbol("smart_component");
+////const sym_options = Symbol("Options");
+const re_valid_typename_chars = /^[a-z0-9_]+$/i;
+
 
 export class SmartComponent extends Events {//{{{
     constructor(
@@ -35,6 +39,59 @@ export class SmartComponent extends Events {//{{{
         if (target) /// FIXME!!!!
         target[sym_smart] = me;
         me.render();
+    };
+    enhance(node) {
+        const me = this;
+
+        // Sanityze and store options:{{{
+        let options = (node.dataset[me.property_name] || "").trim();
+        options = (
+            // Accept 'str' as shorthand for '{type: "str"}':
+            options.match(re_valid_typename_chars) ? {type: options}
+            // Otherwise it must be a valid JSON:
+            : JSON.parse(options)
+        );
+        const name = validName(
+            options.name
+            , node.getAttribute("name")
+        );
+        options.parent = me;
+        ////node[sym_options] = options;
+        //}}}
+
+        // Classify:{{{
+        if (options.action) {
+            options.type ||= "action"; // Make type optional for actions.
+            if (options.type != "action") throw new Error(
+                `Actions must be of type "action" but "${options.type}" given.`
+            );
+            delete options.name; // Actions are always unnamed.
+        } else if (typeof options.type != "string") {
+            throw new Error(
+                `Invalid SmartFom item: type is mandatory for non action elements.`
+            );
+        } else {
+            options.name = name;
+        };
+        //}}}
+
+        // Enhance:{{{
+        const ctrl = me.components[options.type];
+        if (! ctrl) throw new Error([
+            "Unimplemented SmartForm component controller:",
+            options.type,
+        ].join(" "));
+        ///////////////////////////////////////////////////////////
+        console.log("Creating child for " + me.options.name + " with name: " + options.name, node, options);
+        ///////////////////////////////////////////////////////////
+        return new ctrl (
+            node
+            , options
+            , me
+        );
+        //}}}
+
+
     };
     getComponent(target) {
         const me = this;
