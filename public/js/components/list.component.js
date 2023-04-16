6228f27a38a8e9d500f22100bae55c3e022df949
Joan Miquel Torres <joanmi@gmail.com>
2023-04-17 00:43:02 +0200

    Implemented lists' min_items and max_items properties.
    Made min_items = 1 by default, protected against invalid values and
    max_items < min_items (by, if this happens, forcing max_items =
    min_items).

--- public/js/components/list.component.js
+++ public/js/components/list.component.js
@@ -17,6 +17,14 @@ import {SmartComponent} from "../lib/component.js";
 export class list extends SmartComponent {
     render () {//{{{
         const me = this;
+        me.min_items = Math.max(0,
+            typeof me.options.min_items == "number" ? me.options.min_items
+            : 1
+        );
+        me.max_items = Math.max(me.min_items,
+            typeof me.options.max_items == "number" ? me.options.max_items
+            : Infinity
+        );
         me.children = [];
         const numChilds = me.target.children.length;
         if (numChilds != 1) throw me.Error(
@@ -38,6 +46,9 @@ export class list extends SmartComponent {
         if (! tplOptions.type) tplOptions.type = "form"; // Use form as default.
         me.setNodeOptions(me.itemTpl, tplOptions);
         me.itemTpl.remove();
+
+        for(let i=0; i<me.min_items; i++) me.addItem();
+
         return;
     };//}}}
     export() {//{{{
@@ -46,6 +57,9 @@ export class list extends SmartComponent {
     };//}}}
     addItem() {//{{{
         const me = this;
+        if (me.children.length >= me.max_items) throw me.Error(
+            `Cannot add items over max_items boundary`
+        );
         const newItem = me.itemTpl.cloneNode(true);
         me.target.appendChild(newItem);
         const newChild = me.enhance(newItem, {type: "form", name: me.children.length});
@@ -56,6 +70,9 @@ export class list extends SmartComponent {
         ...options
     }) {
         const me = this;
+        if (me.children.length <= me.min_items) throw me.Error(
+            `Cannot remove items under min_items boundary`
+        );
         if (target instanceof Array) return target.map(t=>me.removeItem({target: t, ...options}));
         if (options.keep_non_empty && ! target.isEmpty()) return;
         me.children = me.children
